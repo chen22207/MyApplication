479118b5186c2ec0e296f2edbc28f2b75ad38daa
Protocol <1014988429@qq.com>
2016-03-12 14:53:44 +0800

    ......

--- mynetworklibrary/src/main/java/com/cs/mynetworklibrary/NetRequest.java
+++ mynetworklibrary/src/main/java/com/cs/mynetworklibrary/NetRequest.java
@@ -4,8 +4,12 @@ package com.cs.mynetworklibrary;
  * Created by chenshuai12619 on 2016/2/16 11:07.
  */
 public class NetRequest {
-	public void getRequest() {
-
-	}
+    public void sendRequest(Class clazz, String url) {
+//		Retrofit retrofit = new Retrofit.Builder()
+//				.baseUrl(url)
+//				.addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+//				.addConverterFactory(GsonConverterFactory.create())
+//				.build();
+    }
 
 }
