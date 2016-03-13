9635ce58d10c288f7afc2628c3afb9753e5a23d9
Protocol <1014988429@qq.com>
2016-03-13 22:02:36 +0800

    ....

--- mynetworklibrary/src/main/java/com/cs/mynetworklibrary/NetRequest.java
+++ mynetworklibrary/src/main/java/com/cs/mynetworklibrary/NetRequest.java
@@ -1,15 +1,19 @@
 package com.cs.mynetworklibrary;
 
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+
 /**
  * Created by chenshuai12619 on 2016/2/16 11:07.
  */
-public class NetRequest {
-	public void sendRequest(Class clazz, String url) {
-//		Retrofit retrofit = new Retrofit.Builder()
-//				.baseUrl(url)
-//				.addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-//				.addConverterFactory(GsonConverterFactory.create())
-//				.build();
-	}
+public class NetRequest<T> {
+    public T sendRequest(Class<T> clazz, String url) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(url)
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(CsConverterFactory.create())
+                .build();
+        return retrofit.create(clazz);
+    }
 
 }
